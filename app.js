0229ae6640b03a0d38400f6921faf938bf41745f
GabrielaJeong <fbrkqls94@gmail.com>
2023-07-31 04:13:15 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const indexRouter = require('./routes');
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use ("/api", indexRouter);
+
+app.listen (PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
