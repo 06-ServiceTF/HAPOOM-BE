60dfcf1825727acd96b489dde5c704d36d8bc269
GabrielaJeong <fbrkqls94@gmail.com>
2023-08-02 16:05:28 +0900

    update: 폴더 구조화 및 테스트폴더 생성

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (_, res) => {
+    return res.send("TF6 Hello World");
+  });
+  
 
 app.use ("/api", indexRouter);
 
 app.listen (PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
