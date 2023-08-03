7ae8429d23c756440d45a07586e3e126fec3cb5d
hyeong08 <gudtjq9735@gmail.com>
2023-08-03 15:57:17 +0900

    create: 모델,마이그레이션 추가

---
+++ src/migrations/20230803040648-create-posts.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING(7000),
+      },
+      latitude: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+      longitude: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+      private: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  },
+};
---
+++ src/models/posts.js
@@ -0,0 +1,74 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Comments, {
+        sourceKey: 'postId',
+        foreignKey: 'postId',
+      });
+      this.hasMany(models.Images, {
+        sourceKey: 'postId',
+        foreignKey: 'postId',
+      });
+      this.hasMany(models.Reports, {
+        sourceKey: 'postId',
+        foreignKey: 'postId',
+      });
+      this.hasMany(models.Likes, {
+        sourceKey: 'postId',
+        foreignKey: 'postId',
+      });
+    }
+  }
+  Posts.init(
+    {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING(7000),
+      },
+      latitude: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+      },
+      longitude: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+      },
+      private: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Posts',
+    }
+  );
+  return Posts;
+};
