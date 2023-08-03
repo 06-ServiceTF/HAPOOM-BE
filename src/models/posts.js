b6cc6ddf5a67724f1dfc6ddaad8b0f43829febf8
hyeong08 <118153827+hyeong08@users.noreply.github.com>
2023-08-04 01:32:55 +0900

    update: post모델 코드 추가

--- src/models/posts.js
+++ src/models/posts.js
@@ -53,6 +53,7 @@ module.exports = (sequelize, DataTypes) => {
       private: {
         allowNull: false,
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
