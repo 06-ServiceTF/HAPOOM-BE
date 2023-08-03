f2ff7a35b62b4371af56f4d54b4ef36577047bcd
hyeong08 <gudtjq9735@gmail.com>
2023-08-04 01:25:05 +0900

    update: 메인 게시글 및 좋아요 게시글 조회 조건 추가

--- src/main/main.repository.js
+++ src/main/main.repository.js
@@ -1,18 +1,18 @@
 const { Posts, Likes, sequelize, Sequelize } = require('../models');
 
 class MainRepository {
-  // 메인 게시글 랜덤 9개 조회
   getMain = async () => {
     const getPosts = await Posts.findAll({
+      where: { private: false },
       limit: 9,
       order: Sequelize.literal('RAND()'),
     });
     return getPosts;
   };
 
-  // 좋아요가 많은 게시글 내림차순 조회
   getMainLiked = async () => {
     const likedPosts = await Posts.findAll({
+      where: { private: false },
       attributes: {
         include: [
           [
@@ -29,7 +29,6 @@ class MainRepository {
       limit: 10,
       raw: true, // 결과를 순수한 JSON 객체로 반환
     });
-
     return likedPosts;
   };
 }
