03e7090b451964f552976dce20cb54228aa323d0
hyeong08 <gudtjq9735@gmail.com>
2023-08-05 17:38:26 +0900

    feat: 댓글 작성,조회,수정,삭제 추가

---
+++ src/comments/comment.repository.js
@@ -0,0 +1,35 @@
+const { Users, Posts, Comments } = require('../models');
+const { Op } = require('sequelize');
+
+class CommentRepository {
+  createComment = async (postId, userId, comment) => {
+    const createComment = await Comments.create({ postId, userId, comment });
+    return createComment;
+  };
+
+  getComments = async (postId) => {
+    const getComments = await Comments.findAll({
+      where: { postId },
+      include: { model: Users, attributes: ['nickname', 'userImage'] },
+      order: [['createdAt', 'desc']],
+    });
+    return getComments;
+  };
+
+  updateComment = async (postId, userId, commentId, comment) => {
+    const updateComment = await Comments.update(
+      { comment },
+      { where: { [Op.and]: [{ postId }, { userId }, { commentId }] } }
+    );
+    return updateComment;
+  };
+
+  deleteComment = async (postId, userId, commentId) => {
+    const deleteComment = await Comments.destroy({
+      where: { [Op.and]: [{ postId }, { userId }, { commentId }] },
+    });
+    return deleteComment;
+  };
+}
+
+module.exports = CommentRepository;
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const mainRouter = require('../main/main.route')
 const userprofileRouter = require('../userprofiles/userprofile.route')
+const commentRouster = require('../comments/comment.route')
 
 // 메인화면 관련
 router.use('/main', mainRouter)
 // 유저프로필 관련
-router.use('/userprofile',userprofileRouter)
+router.use('/userprofile', userprofileRouter)
+// 댓글 관련
+router.use('/post', commentRouster)
 
 module.exports = router;
