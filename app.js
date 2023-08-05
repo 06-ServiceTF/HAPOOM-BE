046ddd0007d8c724a67347145382eb27ebaa9aa9
hyeong08 <gudtjq9735@gmail.com>
2023-08-06 00:42:37 +0900

    create: 에러 미들웨어 파일 추가 및 app.js 에러 핸들러 추가

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/api', routes);
 
+//에러 핸들러
+app.use((err, req, res, next) => {
+  const errorMessage = err.stack;
+  console.error('errorMessage:', errorMessage);
+  return res.status(err.status || 400).json({
+    errorMessage: err.message || '오류가 발생했습니다',
+  });
+});
+
 app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`${process.env.PORT || 3000} 포트에 접속 되었습니다.`);
 });
