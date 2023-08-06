e3384428846907bf5cf909b60f05325b268e7844
hyeong08 <gudtjq9735@gmail.com>
2023-08-06 15:01:36 +0900

    update: 유저프로필 조회시 private조건 설정 추가

--- src/userprofiles/userprofile.repository.js
+++ src/userprofiles/userprofile.repository.js
@@ -1,14 +1,31 @@
 const { Posts, Users, Likes, sequelize, Sequelize } = require('../models');
 
 class UserprofileRepository {
-  userPosts = async (userId) => {
+  // 유저가 작성한 게시글 가져오기
+  userPosts = async (userId, loggedInUserId) => {
     const userPosts = await Posts.findAll({
-      where: { userId },
+      where: {
+        userId,
+        private: false, // public 게시글만 가져옵니다.
+      },
       include: { model: Users, attributes: ['nickname', 'userImage'] },
     });
+
+    // 프로필 주인인 경우, private 게시글도 가져옵니다.
+    if (userId === loggedInUserId) {
+      const privatePosts = await Posts.findAll({
+        where: {
+          userId,
+          private: true,
+        },
+        include: { model: Users, attributes: ['nickname', 'userImage'] },
+      });
+      userPosts.push(...privatePosts);
+    }
     return userPosts;
   };
 
+  // 유저가 좋아요를 누른 게시글 가져오기
   userLikedPosts = async (userId) => {
     const likedPosts = await Posts.findAll({
       include: [
@@ -18,7 +35,10 @@ class UserprofileRepository {
         },
       ],
     });
-    return likedPosts;
+
+    // private 게시글은 제외하여 필터링합니다.
+    const filteredLikedPosts = likedPosts.filter((post) => !post.private);
+    return filteredLikedPosts;
   };
 }
 
--- src/userprofiles/userprofile.service.js
+++ src/userprofiles/userprofile.service.js
@@ -3,8 +3,8 @@ const UserprofileRepository = require('./userprofile.repository');
 class UserprofileService {
   userprofileRepository = new UserprofileRepository();
 
-  userprofile = async (userId) => {
-    const userPosts = await this.userprofileRepository.userPosts(userId);
+  userprofile = async (userId, loggedInUserId) => {
+    const userPosts = await this.userprofileRepository.userPosts(userId, loggedInUserId);
     const userLikedPosts = await this.userprofileRepository.userLikedPosts(userId);
 
     return {
