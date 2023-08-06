e6f9f6425895f57a016755d9ea6a360c8555d39e
hyeong08 <gudtjq9735@gmail.com>
2023-08-06 20:02:04 +0900

    style: index.route 오타 수정

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const mainRouter = require('../main/main.route')
 const userprofileRouter = require('../userprofiles/userprofile.route')
-const commentRouster = require('../comments/comment.route')
+const commentRouter = require('../comments/comment.route')
 
 // 메인화면 관련
 router.use('/main', mainRouter)
 // 유저프로필 관련
 router.use('/userprofile', userprofileRouter)
 // 댓글 관련
-router.use('/post', commentRouster)
+router.use('/post', commentRouter)
 
 module.exports = router;
