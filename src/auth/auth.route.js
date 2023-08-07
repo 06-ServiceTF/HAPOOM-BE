5dfe04e922b59add8ff7ce18791a727205d1394b
GabrielaJeong <fbrkqls94@gmail.com>
2023-08-07 17:25:26 +0900

    feat: 패스포트 및 JWT 2차 수정

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -8,62 +8,35 @@ const LoginInstance = new LoginController();
 
 const router = express.Router();
 
+const setJwtCookie = (req, res) => {
+  const user = req.user;
+
+  if (!user || !user.userId) {
+    return res.status(401).json({ message: "로그인에 실패했습니다." });
+  }
+
+  const token= jwt.sign ({ userId: user.userId }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("skycookie", `Bearer ${token}`);
+
+  return res.status(200).json({ message: "로그인 성공", token });
+}
+
 router.post("/register", RegisterInstance.registerUser);
 router.post("/login", LoginInstance.loginUser);
 
+router.get("/auth-failed", (req, res) => {
+  res.status(401).json({ message: "로그인에 실패했습니다." })});;
 
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect : "/login"}),
-  (req, res) => {
-    const user = req.user;
+router.get("/google/callback", passport.authenticate("google", { failureRedirect : "/auth-failed"}), setJwtCookie);
 
-    //JWT 토큰 생성
-    const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    })
+router.get("/naver", passport.authenticate("naver"));
+router.get("/naver/callback", passport.authenticate("naver", { failureRedirect : "/auth-failed"}), setJwtCookie);
 
-    res.cookie("skycookie", `Bearer ${token}`);
-
-    return res.status(200).json({ message: "로그인 성공", token });
-  }
-);
-
-router.get("/naver", passport.authenticate("naver")); // Naver로 변경
-router.get(
-  "/naver/callback",
-  passport.authenticate("naver", { failureRedirect : "/login"}),
-  (req, res) => {
-    const user = req.user;
-
-    //JWT 토큰 생성
-    const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    })
-
-    res.cookie("skycookie", `Bearer ${token}`);
-
-    return res.status(200).json({ message: "로그인 성공", token });
-  }
-);
-
-router.get("kakao", passport.authenticate("kakao"));
-router.get(
-  "/kakao/callback",
-  passport.authenticate("kakao", { failureRedirect : "/login"}),
-  (req, res) => {
-    const user = req.user;
-
-    //JWT 토큰 생성
-    const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    })
-
-    res.cookie("skycookie", `Bearer ${token}`);
-
-    return res.status(200).json({ message: "로그인 성공", token });
-  }
-);
+router.get("/kakao", passport.authenticate("kakao"));
+router.get("/kakao/callback", passport.authenticate("kakao", { failureRedirect : "/auth-failed"}), setJwtCookie);
 
 module.exports = router;
