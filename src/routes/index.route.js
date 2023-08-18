a21813fe0aea395e7021fa6b7ba1aeb99cb2f47f
hyeong08 <gudtjq9735@gmail.com>
2023-08-18 10:36:13 +0900

    fix: follow 기능 주석처리

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -9,14 +9,14 @@ const reportRouter = require('../reports/report.route');
 const authRouter = require('../auth/auth.route');
 const postRouter = require('../posts/post.route');
 const utilRouter = require('../util/util.route');
-const followRouter = require('../follows/follow.route')
+// const followRouter = require('../follows/follow.route')
 
 // 로그인 회원가입 관련
 router.use('/auth', authRouter);
 // 메인화면 관련
 router.use('/main', mainRouter);
 // 유저 프로필, 팔로우 관련
-router.use('/user', [userRouter, followRouter]);
+router.use('/user', userRouter);
 // 게시글, 댓글, 좋아요 관련
 router.use('/post', [postRouter, commentRouter, likeRouter]);
 // 신고 관련
