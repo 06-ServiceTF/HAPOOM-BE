f9e3843dda8cd02833328ad4c9d59a13f3d022fb
fish-minkyu <e951219@naver.com>
2023-08-18 18:22:08 +0900

    update: profile.route.js 코드 수정

--- src/profiles/profile.route.js
+++ src/profiles/profile.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authMiddleware = require('../middlewares/auth.middleware');
-const multerMiddleware = require('../middlewares/multer.middleware');
+const { multerMiddleware } = require('../middlewares/multer.middleware');
 const ProfileController = require('./profile.controller');
 // const multer = require('multer');
 // const fs = require('fs');
@@ -32,10 +32,15 @@ const profileController = new ProfileController();
 //   { name: 'audio', maxCount: 1 },
 // ]);
 
+const upload = multerMiddleware.fields([
+  { name: 'image', maxCount: 5 },
+  { name: 'audio', maxCount: 1}
+])
+
 // 유저 정보 조회
 router.get('/', authMiddleware, profileController.userInfo);
 // 유저 정보 수정
-router.patch('/', authMiddleware, multerMiddleware, profileController.updateUser);
+router.patch('/', authMiddleware, upload, profileController.updateUser);
 // 마이페이지 조회
 router.get('/myprofile', authMiddleware, profileController.myProfile);
 // 유저페이지 조회
