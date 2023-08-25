048a8598dd156e55ae282d942199a2ed5e8e30a1
hyeong08 <gudtjq9735@gmail.com>
2023-08-25 21:50:29 +0900

    update: Subscriptons 테이블 추가

---
+++ src/migrations/20230825122200-create-subscription.js
@@ -0,0 +1,29 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Subscriptions', {
+      endpoint: {
+        type: Sequelize.STRING,
+      },
+      expirationTime: {
+        type: Sequelize.STRING,
+      },
+      keys: {
+        type: Sequelize.JSON,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Subscriptions');
+  },
+};
---
+++ src/models/subscription.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Subscription extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Subscription.init(
+    {
+      endpoint: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      expirationTime: DataTypes.STRING,
+      keys: {
+        type: DataTypes.JSON,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Subscription',
+    }
+  );
+  return Subscription;
+};
