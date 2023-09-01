cd2b3a98d47d8fa6b4ed8e3f6a2f87c089f1fe5a
TheON2 <sniperad@naver.com>
2023-09-01 09:54:37 +0900

    Update: limiter middleware

--- src/posts/post.route.js
+++ src/posts/post.route.js
@@ -3,14 +3,18 @@ const { multerMiddleware } = require('../middlewares/multer.middleware')
 const PostsController = require('./post.controller');
 const router = express.Router();
 const postsController = new PostsController();
+const { isIPBlocked, limiter,postlimiter } = require('../middlewares/limiter.middleware');
 
 const upload = multerMiddleware.fields([
   { name: 'image', maxCount: 5 },
   { name: 'audio', maxCount: 1}
-])
+]);
 
-router.post('', upload, postsController.createPost);
-router.get('/:postId', postsController.getPost);
+// // 미들웨어를 라우터의 시작 부분에서 적용
+// router.use(isIPBlocked, limiter);
+
+router.post('', upload, isIPBlocked, postlimiter,postsController.createPost);
+router.get('/:postId',isIPBlocked, limiter, postsController.getPost);
 router.put('/:postId', upload, postsController.updatePost);
 router.delete('/:postId', postsController.deletePost);
 
